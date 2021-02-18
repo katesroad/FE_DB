59e8c5b12e9aa29ba1449463d5d4d35975265d50
Kate <test@gmail.com>
2021-02-18 10:50:59 -0500

    test search country by region

--- countries/cypress/integration/home.js
+++ countries/cypress/integration/home.js
@@ -1,5 +1,5 @@
-describe("home page", () => {
-	it.only("search existing country by name", () => {
+describe("home page integration testing", () => {
+	it("search country by country name", () => {
 		cy.visit("/");
 
 		// show loading spinner
@@ -11,14 +11,24 @@ describe("home page", () => {
 			form.find("button").click();
 		});
 		cy.findByRole("heading", "China");
-	});
-	it.only("search not existing country", () => {
-		cy.visit("/");
-		cy.findByLabelText("loading");
-		cy.findByRole("textbox").type("Unknown country");
+
+		cy.findByRole("textbox").clear().type("Unknown country");
 		cy.get("form").within((form) => {
 			form.find("button").click();
 		});
 		cy.findByText(/not found/i);
+	});
+	it.only("search country by region", () => {
+		cy.visit("/");
+
+		// search country by region name
+		cy.findByRole("button", { name: /filter by region/i }).click();
+		cy.findByRole("option", { name: /africa/i }).click();
+		cy.findByText(/Algeria/i);
+
+		// reset country region as all
+		cy.findByRole("button", { name: /africa/i }).click();
+		cy.findByRole("option", { name: /filter by region/i }).click();
+		cy.findByRole("heading", { name: /Afghanistan/i });
 	});
 });
