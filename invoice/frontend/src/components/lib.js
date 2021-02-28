d2b40ccca22bb2e242fd544e3815d2b5bca39660
Kate <test@gmail.com>
2021-02-28 14:48:57 -0500

    <Card /> for UI lib

--- invoice/frontend/src/components/lib.js
+++ invoice/frontend/src/components/lib.js
@@ -46,3 +46,10 @@ export const Button = styled(ButtonBase)(
     },
   })
 );
+
+export const Card = styled.div`
+  padding: 24px;
+  border-radius: 8px;
+  box-shadow: 0px 10px 10px -10px rgba(72, 84, 159, 0.100397);
+  background-color: var(--element-background-color);
+`;
