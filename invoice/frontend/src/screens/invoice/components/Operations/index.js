53f8e38617baf847b85db34cef5e20384dca5c83
Kate <test@gmail.com>
2021-03-06 14:33:27 -0500

    add mark as pending button

--- invoice/frontend/src/screens/invoice/components/Operations/index.js
+++ invoice/frontend/src/screens/invoice/components/Operations/index.js
@@ -5,6 +5,7 @@ import { Button } from "components/lib";
 import MarkButton from "components/MarkButton";
 import PropTypes from "prop-types";
 import DeleteBtn from "../ConfirmDeletion";
+import { useUpdateInvoice } from "hooks/invoice-hooks";
 
 function EditButton({ id }) {
   const handleClick = (e) => {
@@ -26,25 +27,48 @@ EditButton.propTypes = {
   id: PropTypes.string.isRequired,
 };
 
+function MarkPaid({ id }) {
+  const { status, mutate } = useUpdateInvoice();
+  return (
+    <Button
+      onClick={() => mutate({ id, status: "paid" })}
+      css={`
+        margin: 0 8px;
+      `}
+      variant="primary"
+    >
+      {status === "pending" ? "marking" : "mark as paid"}
+    </Button>
+  );
+}
+
+function MarkPending({ id }) {
+  const { status, mutate } = useUpdateInvoice();
+  return (
+    <Button onClick={() => mutate({ id, status: "pending" })} variant="primary">
+      {status === "pending" ? "marking" : "mark as pending"}
+    </Button>
+  );
+}
+
 /**
  * mark as paid, delete, and edit an invoice based on its status
  * An invoice is editable when the status is draft
  * An invoice can be marked as paid when the status is pending
  * An invoice can be deleted when the status is either draft or pending
  */
-function Operations({ status, ...invoice }) {
-  if (status === "paid") return <DeleteBtn {...invoice} />;
-  return (
-    <p>
-      {status === "draft" ? (
-        <>
-          <EditButton id={invoice.id} />
-          <MarkButton id={invoice.id} />
-        </>
-      ) : null}
-      {status === "pending" ? <MarkButton id={invoice.id} /> : null}
-    </p>
-  );
+function Operations({ status, id, tag }) {
+  const { status: markingStatus, mutate } = useUpdateInvoice();
+  if (status === "paid") return <DeleteBtn id={id} tag={tag} />;
+  if (status === "draft")
+    return (
+      <>
+        <EditButton id={id} />
+        <MarkPending id={id} />
+        <MarkPaid id={id} />
+      </>
+    );
+  if (status == "pending") return <MarkPaid id={id} />;
 }
 Operations.propTypes = {
   id: PropTypes.string.isRequired,
