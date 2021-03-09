693c32c206229ad349fa67a58e47c53031af5ef0
Kate <test@gmail.com>
2021-03-09 15:20:00 -0500

    remove file should not be tracked

--- invoice/spa/src/styles/fonts.js
+++
@@ -1,6 +0,0 @@
-// font weight settting
-export const fontWeightNormal = 400;
-export const fontWeightBold = 600;
-export const fontWeightBolder = 700;
-
-// font size setting
--- invoice/spa/src/styles/media-queries.js
+++
@@ -1,5 +0,0 @@
-export const iphone = "@media (min-width:375px)";
-export const small = "@media (min-width: 576px)";
-export const medium = "@media (min-width: 768px)";
-export const large = "@media(min-width:992px)";
-export const xlarge = "@media(min-width:1200px)";
