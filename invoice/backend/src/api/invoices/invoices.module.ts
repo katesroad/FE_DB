4878113a60195da8171c0fdec2e0899da65510c3
Kate <test@gmail.com>
2021-03-11 12:12:27 -0500

    generate invoice tag at backend side

--- invoice/backend/src/api/invoices/invoices.module.ts
+++ invoice/backend/src/api/invoices/invoices.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongoModule } from 'common/mongo';
 import { InvoicesController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
+import { HelperService } from './helper.service';
 
 @Module({
   imports: [MongoModule],
   controllers: [InvoicesController],
-  providers: [InvoicesService],
+  providers: [InvoicesService, HelperService],
 })
 export class InvoicesModule {}
