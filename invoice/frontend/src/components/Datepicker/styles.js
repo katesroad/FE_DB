fdf674b10e41c763b55974e9307f4e99b298674a
Kate <test@gmail.com>
2021-03-11 20:14:41 -0500

    using style extract from lib

--- invoice/frontend/src/components/Datepicker/styles.js
+++ invoice/frontend/src/components/Datepicker/styles.js
@@ -1,52 +1,17 @@
 import styled from "styled-components";
-import { ButtonBase, FormControl } from "components/lib";
 import ReactDatepicker from "react-datepicker";
+import { inputStyle } from "components/lib/form";
+import { buttonStyle } from "components/lib/button";
 require("react-datepicker/dist/react-datepicker.css");
 
 export const DatepickerCompo = styled(ReactDatepicker)`
-  padding: 8px 16px;
-  height: 48px;
-  max-width: 130px;
-  border: 1px solid;
-  border-color: var(--input-border-color);
-  box-sizing: border-box;
-  border-radius: 4px;
-  font-size: 16px;
-  color: var(--input-text-color);
-  background-color: var(--input-background-color);
-  &:disabled {
-    width: auto;
-    border: 0;
-    background-color: transparent;
-  }
-  &:focus {
-    border: 1px solid var(--color-primary);
-  }
+  ${inputStyle}
+  max-width: 100px;
 `;
 
 export const Button = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px 16px;
-  height: 48px;
-  max-width: 140px;
-  border: 1px solid;
-  border-color: var(--input-border-color);
-  box-sizing: border-box;
-  border-radius: 4px;
-  font-size: 16px;
-  color: var(--input-text-color);
-  background-color: var(--input-background-color);
-  cursor: pointer;
-  &:disabled {
-    width: auto;
-    border: 0;
-    background-color: transparent;
-  }
-  &:focus {
-    border: 1px solid var(--color-primary);
-  }
+  ${buttonStyle}
+  display: block;
 `;
 
 export const DateValue = styled.span`
