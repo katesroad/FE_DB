022001fa401cd195e4927ffb5d8967378b6461bd
Kate <test@gmail.com>
2021-03-13 20:21:39 -0500

    remove not used code segment

--- invoice/frontend/src/screens/invoices/components/CreateInvoice/styles.js
+++ invoice/frontend/src/screens/invoices/components/CreateInvoice/styles.js
@@ -2,15 +2,6 @@ import styled from "styled-components";
 import { buttonStyle } from "components/lib/button";
 import * as mediaQueries from "styles/media-queries";
 
-export const Wrapper = styled.section`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 32px;
-  ${mediaQueries.medium} {
-    margin-bottom: 56px;
-  }
-`;
 export const InvoiceText = styled.span`
   display: none;
   ${mediaQueries.small} {
