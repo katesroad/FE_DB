d8c71355f57fb9a9c699c08a7c5fdfe14f5608c1
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-18 23:41:32 -0400

    Update index.js

--- job/src/screens/jobs/index.js
+++ job/src/screens/jobs/index.js
@@ -11,74 +11,74 @@ import { Content } from "components/lib";
 
 // Invoice detail page
 export default function JobsScreen() {
-	const [filter, setFilter] = React.useState({
-		description: "",
-		location: "",
-		full_time: false,
-	});
+  const [filter, setFilter] = React.useState({
+    description: "",
+    location: "",
+    full_time: false,
+  });
 
-	// job search happens after form submmiting triggered
-	const [doSearch, setDoSearch] = React.useState(true);
-	const [params, setParams] = React.useState({});
-	React.useEffect(() => doSearch && setParams(filter), [filter, doSearch]);
+  // job search happens after form submmiting triggered
+  const [doSearch, setDoSearch] = React.useState(true);
+  const [params, setParams] = React.useState({});
+  React.useEffect(() => doSearch && setParams(filter), [filter, doSearch]);
 
-	const { status, data: jobs, error } = useGetJobs(params);
+  const { status, data: jobs, error } = useGetJobs(params);
 
-	const handleChange = (update) => {
-		setDoSearch(false);
-		setFilter({ ...filter, ...update });
-	};
-	const handleSubmit = () => setDoSearch(true);
-	return (
-		<>
-			<AppHeader>
-				<Content>
-					<ThemeSwitcher
-						css={`
-							paddint-top: 32px;
-							padding-bottom: 32px;
-							${mediaQueries.medium} {
-								padding-top: 42px;
-								padding-bottom: 46px;
-							}
-							${mediaQueries.large} {
-								padding-top: 45px;
-								padding-bottom: 45px;
-							}
-						`}
-					/>
-					<JobFilter
-						{...filter}
-						onChange={handleChange}
-						onSubmit={handleSubmit}
-					/>
-				</Content>
-			</AppHeader>
-			<AppMain>
-				<div
-					css={`
-						padding-top: 32px;
-						${mediaQueries.small} {
-							margin-top: calc(40px + 1vw);
-						}
-						${mediaQueries.large} {
-							margin-top: 56px;
-						}
-					`}
-				>
-					{status === "success" ? (
-						<JobList jobs={jobs} />
-					) : (
-						<>
-							{status === "error" ? (
-								<p>{JSON.stringify(error)}</p>
-							) : (
-								<p>Loading...</p>
-							)}
-						</>
-					)}
-				</div>
-			</AppMain>
-		</>
-	);
+  const handleChange = (update) => {
+    setDoSearch(false);
+    setFilter({ ...filter, ...update });
+  };
+  const handleSubmit = () => setDoSearch(true);
+  return (
+    <>
+      <AppHeader>
+        <Content>
+          <ThemeSwitcher
+            css={`
+              paddint-top: 32px;
+              padding-bottom: 32px;
+              ${mediaQueries.medium} {
+                padding-top: 42px;
+                padding-bottom: 46px;
+              }
+              ${mediaQueries.large} {
+                padding-top: 45px;
+                padding-bottom: 45px;
+              }
+            `}
+          />
+          <JobFilter
+            {...filter}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        </Content>
+      </AppHeader>
+      <AppMain>
+        <div
+          css={`
+            padding-top: 32px;
+            ${mediaQueries.small} {
+              margin-top: calc(40px + 1vw);
+            }
+            ${mediaQueries.large} {
+              margin-top: 56px;
+            }
+          `}
+        >
+          {status === "success" ? (
+            <JobList jobs={jobs} />
+          ) : (
+            <>
+              {status === "error" ? (
+                <p>{JSON.stringify(error)}</p>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </>
+          )}
+        </div>
+      </AppMain>
+    </>
+  );
 }
