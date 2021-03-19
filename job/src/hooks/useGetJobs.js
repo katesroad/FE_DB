b8b8d3db96daf5f19e4c05c36fdaeb7121d42bf3
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-19 19:32:40 -0400

    Update useGetJobs.js

--- job/src/hooks/useGetJobs.js
+++ job/src/hooks/useGetJobs.js
@@ -2,20 +2,20 @@ import { useQuery } from "react-query";
 import axios from "utils/axios";
 
 export function useGetJobs(filter) {
-	const params = { ...filter };
-	Object.keys(params).forEach((key) => {
-		if (!params[key]) delete params[key];
-		else params[key] = encodeURI(params[key]);
-	});
-	return useQuery({
-		queryFn: () => axios.get("/positions.json", { params }),
-		queryKey: ["jobs", params],
-	});
+  const params = { ...filter };
+  Object.keys(params).forEach((key) => {
+    if (!params[key]) delete params[key];
+    else params[key] = encodeURI(params[key]);
+  });
+  return useQuery({
+    queryFn: () => axios.get("/positions.json", { params }),
+    queryKey: ["jobs", params],
+  });
 }
 
 export function useGetJob(id) {
-	return useQuery({
-		queryFn: () => axios.get(`/positions/${id}.json`),
-		queryKey: ["job", id],
-	});
+  return useQuery({
+    queryFn: () => axios.get(`/positions/${id}.json`),
+    queryKey: ["job", id],
+  });
 }
