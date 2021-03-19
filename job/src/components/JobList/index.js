700df3610864012b2c7349d53e29e8b2d1926633
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-19 19:30:11 -0400

    Update index.js

--- job/src/components/JobList/index.js
+++ job/src/components/JobList/index.js
@@ -6,52 +6,52 @@ import * as React from "react";
 import { JobWrap, Logo, Content } from "./styles";
 
 export const JobType = {
-	id: PropTypes.string.isRequired,
-	company_logo: PropTypes.string,
-	created_at: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	company: PropTypes.string.isRequired,
-	location: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  company_logo: PropTypes.string,
+  created_at: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
 };
 
 export const JobItem = React.memo(({ ...job }) => (
-	<JobWrap to={`/job/${job.id}`}>
-		<Logo style={{ backgroundImage: `url(${job.company_logo})` }} />
-		<Content>
-			<Typography variant="body" as="p">
-				<span>{new Date(job.created_at).toLocaleDateString()}</span>
-				<span className="dot">.</span>
-				<span>{job.type}</span>
-			</Typography>
-			<Typography variant="h3" as="h3" className="title">
-				{job.title}
-			</Typography>
-			<Typography variant="body" as="p" className="company">
-				<span>{job.company}</span>
-			</Typography>
-			<p className="location">{job.location}</p>
-		</Content>
-	</JobWrap>
+  <JobWrap to={`/job/${job.id}`}>
+    <Logo style={{ backgroundImage: `url(${job.company_logo})` }} />
+    <Content>
+      <Typography variant="body" as="p">
+        <span>{new Date(job.created_at).toLocaleDateString()}</span>
+        <span className="dot">.</span>
+        <span>{job.type}</span>
+      </Typography>
+      <Typography variant="h3" as="h3" className="title">
+        {job.title}
+      </Typography>
+      <Typography variant="body" as="p" className="company">
+        <span>{job.company}</span>
+      </Typography>
+      <p className="location">{job.location}</p>
+    </Content>
+  </JobWrap>
 ));
 JobItem.propTypes = { ...JobType };
 
 const JobList = ({ jobs }) => (
-	<div
-		css={`
-			width: 100%;
-			display: grid;
-			grid-template-columns: repeat(auto-fill, minmax(286px, 1fr));
-			grid-auto-rows: minmax(228px, auto);
-			gap: 1.5rem;
-		`}
-	>
-		{jobs?.map((job) => (
-			<JobItem {...job} key={job.id} />
-		))}
-	</div>
+  <div
+    css={`
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(286px, 1fr));
+      grid-auto-rows: minmax(228px, auto);
+      gap: 1.5rem;
+    `}
+  >
+    {jobs?.map((job) => (
+      <JobItem {...job} key={job.id} />
+    ))}
+  </div>
 );
 JobList.propTypes = {
-	jobs: PropTypes.arrayOf(PropTypes.shape(JobType)),
+  jobs: PropTypes.arrayOf(PropTypes.shape(JobType)),
 };
 
 export default JobList;
