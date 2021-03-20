3307eb6edeb7112dafcc54d1d343c7ec2c9c16bf
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-20 00:33:51 -0400

    Update styles.js

--- job/src/components/JobList/styles.js
+++ job/src/components/JobList/styles.js
@@ -4,48 +4,48 @@ import { colors } from "styles/colors";
 
 /*--------------------styling for job item--------------------------*/
 export const JobWrap = styled(Link)`
-	position: relative;
-	display: block;
-	padding: 32px;
-	margin-bottom: 1.5rem;
-	border-radius: 6px;
-	color: inherit;
-	background: var(--element-background);
+  position: relative;
+  display: block;
+  padding: 32px;
+  margin-bottom: 1.5rem;
+  border-radius: 6px;
+  color: inherit;
+  background: var(--element-background);
 `;
 
 export const Content = styled.div`
-	display: flex;
-	flex-direction: column;
-	padding-top: 20px;
-	.title {
-		margin-top: 11px;
-		margin-bottom: 10px;
-	}
-	.company {
-		margin-bottom: 36px;
-	}
-	.dot {
-		margin-left: 12px;
-		margin-right: 12px;
-	}
-	.location {
-		justify-self: flex-end;
-		font-size: 14px;
-		line-height: 18px;
-		color: ${colors.p00};
-		font-weight: bold;
-	}
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  .title {
+    margin-top: 11px;
+    margin-bottom: 10px;
+  }
+  .company {
+    margin-bottom: 36px;
+  }
+  .dot {
+    margin-left: 12px;
+    margin-right: 12px;
+  }
+  .location {
+    justify-self: flex-end;
+    font-size: 14px;
+    line-height: 18px;
+    color: ${colors.p00};
+    font-weight: bold;
+  }
 `;
 
 export const Logo = styled.div`
-	position: absolute;
-	top: -25px;
-	left: 32px;
-	border-radius: 15px;
-	height: 50px;
-	width: 50px;
-	box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
-	background-repeat: no-repeat;
-	background-position: center;
-	background-size: contain;
+  position: absolute;
+  top: -25px;
+  left: 32px;
+  border-radius: 15px;
+  height: 50px;
+  width: 50px;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
 `;
