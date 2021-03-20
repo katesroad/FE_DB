1acc36772804d630bcb1371e4b5cf531cdb54867
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-20 00:34:18 -0400

    Update styles.js

--- job/src/components/header/styles.js
+++ job/src/components/header/styles.js
@@ -6,19 +6,19 @@ import HeaderBgDesktop from "./bg.header.desktop.svg";
 
 /*-----------------Styles for App Header------------------------------*/
 export const Header = styled.header`
-	height: 136px;
-	border-radius: 0 0 0 100px;
-	background-image: url(${HeaderBgSmall});
-	background-size: cover;
-	background-position: center top;
-	background-repeat: no-repeat;
-	z-index: 1000;
-	${mediaQueries.medium} {
-		height: 160px;
-		border-radius: 0px 0px 0 100px;
-		background-image: url(${HeaderBgMedium});
-	}
-	${mediaQueries.large} {
-		background-image: url(${HeaderBgDesktop});
-	}
+  height: 136px;
+  border-radius: 0 0 0 100px;
+  background-image: url(${HeaderBgSmall});
+  background-size: cover;
+  background-position: center top;
+  background-repeat: no-repeat;
+  z-index: 1000;
+  ${mediaQueries.medium} {
+    height: 160px;
+    border-radius: 0px 0px 0 100px;
+    background-image: url(${HeaderBgMedium});
+  }
+  ${mediaQueries.large} {
+    background-image: url(${HeaderBgDesktop});
+  }
 `;
