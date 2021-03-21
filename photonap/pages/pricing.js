76e727839c730614bf9e612e98afcee76bfcbd1a
Kate <test@gmail.com>
2021-03-21 18:47:32 -0400

    hero picture for pricing page

--- photonap/pages/pricing.js
+++ photonap/pages/pricing.js
@@ -1,10 +1,37 @@
+import styled from "styled-components";
 import { Content } from "components/lib";
 import * as React from "react";
+import * as mediaQueries from "styles/media-queries";
+import PageHero from "components/PageHero";
+
+const intro = {
+	title: "pricing",
+	content:
+		"Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.",
+};
 
 export default function PricingScreen() {
 	return (
-		<Content as="main">
-			<h2>Pricinng page</h2>
-		</Content>
+		<>
+			<PageHero
+				{...intro}
+				css={`
+					.hero-img {
+						background-image: url(/pricing/mobile/hero.jpg);
+					}
+					${mediaQueries.medium} {
+						.hero-img {
+							background-image: url(/pricing/tablet/hero.jpg);
+						}
+					}
+					${mediaQueries.large} {
+						.hero-img {
+							background-image: url(/pricing/desktop/hero.jpg);
+						}
+					}
+				`}
+			/>
+			<Content as="main"></Content>
+		</>
 	);
 }
