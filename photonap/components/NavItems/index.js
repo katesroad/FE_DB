10047ff5c651fd520a805ecf3a0c17ab2f9fc7a9
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-22 16:15:31 -0400

    Update index.js

--- photonap/components/NavItems/index.js
+++ photonap/components/NavItems/index.js
@@ -3,35 +3,35 @@ import * as React from "react";
 import Link from "next/link";
 
 export default function NavItems({ children, ...props }) {
-	return (
-		<ul
-			css={`
-				li {
-					width: 100%;
-					text-transform: uppercase;
-				}
-				a {
-					display: block;
-					width: 100%;
-				}
-				// the invite button
-				button {
-					width: 100%;
-				}
-			`}
-			{...props}
-		>
-			<li>
-				<Link href="/stories">stories</Link>
-			</li>
-			<li>
-				<Link href="/features">features</Link>
-			</li>
-			<li>
-				<Link href="/pricing">pricing</Link>
-			</li>
-			{/* to place the invite button for header */}
-			{children}
-		</ul>
-	);
+  return (
+    <ul
+      css={`
+        li {
+          width: 100%;
+          text-transform: uppercase;
+        }
+        a {
+          display: block;
+          width: 100%;
+        }
+        // the invite button
+        button {
+          width: 100%;
+        }
+      `}
+      {...props}
+    >
+      <li>
+        <Link href="/stories">stories</Link>
+      </li>
+      <li>
+        <Link href="/features">features</Link>
+      </li>
+      <li>
+        <Link href="/pricing">pricing</Link>
+      </li>
+      {/* to place the invite button for header */}
+      {children}
+    </ul>
+  );
 }
