07ee1b4fcd51a57bd111996b7802dfbddfc1a24a
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-22 16:12:44 -0400

    Update index.js

--- photonap/components/TierCompare/index.js
+++ photonap/components/TierCompare/index.js
@@ -5,53 +5,53 @@ import { FEATURE_PRICING } from "./features";
 import { Wrapper, FeatureWrap, FeatureTiers } from "./styles";
 
 const FeatureIntro = ({ name, tiers }) => (
-	<FeatureWrap as="li">
-		<h4 className="feature-name">{name}</h4>
-		<FeatureTiers>
-			{tiers.map(({ name, covered }) => (
-				<p key={name}>
-					<span className="tier-name">{name}</span>
-					{covered ? (
-						<span className={`icon-check tier-${name}`}>
-							<IconCheck />
-						</span>
-					) : (
-						<span />
-					)}
-				</p>
-			))}
-		</FeatureTiers>
-	</FeatureWrap>
+  <FeatureWrap as="li">
+    <h4 className="feature-name">{name}</h4>
+    <FeatureTiers>
+      {tiers.map(({ name, covered }) => (
+        <p key={name}>
+          <span className="tier-name">{name}</span>
+          {covered ? (
+            <span className={`icon-check tier-${name}`}>
+              <IconCheck />
+            </span>
+          ) : (
+            <span />
+          )}
+        </p>
+      ))}
+    </FeatureTiers>
+  </FeatureWrap>
 );
 FeatureIntro.propTypes = {
-	name: PropTypes.string.isRequired,
-	tiers: PropTypes.arrayOf(
-		PropTypes.shape({
-			name: PropTypes.oneOf(["basic", "pro", "business"]).isRequired,
-			covered: PropTypes.bool.isRequired,
-		})
-	).isRequired,
+  name: PropTypes.string.isRequired,
+  tiers: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.oneOf(["basic", "pro", "business"]).isRequired,
+      covered: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
 };
 
 // pricing tier compare
 export default function TireCompare() {
-	return (
-		<Wrapper>
-			<h2 className="title title--desktop">Compare</h2>
-			<div className="title title--tablet">
-				<span>The features</span>
-				<div className="tier-types">
-					<span>basic</span>
-					<span>pro</span>
-					<span>business</span>
-				</div>
-			</div>
-			<h4 className="title title--mobile"> The features</h4>
-			<ul>
-				{FEATURE_PRICING.map((feature) => (
-					<FeatureIntro key={feature.name} {...feature} />
-				))}
-			</ul>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <h2 className="title title--desktop">Compare</h2>
+      <div className="title title--tablet">
+        <span>The features</span>
+        <div className="tier-types">
+          <span>basic</span>
+          <span>pro</span>
+          <span>business</span>
+        </div>
+      </div>
+      <h4 className="title title--mobile"> The features</h4>
+      <ul>
+        {FEATURE_PRICING.map((feature) => (
+          <FeatureIntro key={feature.name} {...feature} />
+        ))}
+      </ul>
+    </Wrapper>
+  );
 }
