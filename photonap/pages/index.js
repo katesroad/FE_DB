89e42c6dbcaff2dd02747c85f92b64c87dbefdf5
Kate <test@gmail.com>
2021-03-22 23:17:55 -0400

    extract feature list to a sinlge component for home page usage

--- photonap/pages/index.js
+++ photonap/pages/index.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
+import { STORIES } from "constant/stories";
+import StoryList from "components/StoryList";
+import FeatureList from "components/FeatureList";
+import { features } from "constant/features";
+
+const stories = STORIES.slice(0, 4);
+const homeFeatures = features.slice(0, 3);
 
 export default function Home() {
-	return <div>My page</div>;
+	return (
+		<div
+			css={`
+				max-width: 1440px;
+				margin-left: auto;
+				margin-right: auto;
+			`}
+		>
+			<StoryList stories={stories} />
+			<FeatureList features={homeFeatures} />
+		</div>
+	);
 }
