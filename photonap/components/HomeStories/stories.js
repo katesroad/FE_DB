ca0244ab539c717672532ffd32b1d1ee44a1272e
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:46:50 -0400

    Update stories.js

--- photonap/components/HomeStories/stories.js
+++ photonap/components/HomeStories/stories.js
@@ -1,25 +1,21 @@
 export const stories = [
-	{
-		title: "Create and share your photo stories.",
-		content:
-			"Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
-		name: "create-and-share.jpg",
-		variant: "dark",
-	},
-	{
-		title: "BEAUTIFUL STORIES EVERYTIME",
-		content:
-			"We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
-		name: "beautiful-stories.jpg",
-	},
-	{
-		title: "DESIGNED FOR EVERYONE",
-		content:
-			"Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
-		name: "designed-for-everyone.jpg",
-	},
+  {
+    title: "Create and share your photo stories.",
+    content:
+      "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+    name: "create-and-share.jpg",
+    variant: "dark",
+  },
+  {
+    title: "BEAUTIFUL STORIES EVERYTIME",
+    content:
+      "We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
+    name: "beautiful-stories.jpg",
+  },
+  {
+    title: "DESIGNED FOR EVERYONE",
+    content:
+      "Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.",
+    name: "designed-for-everyone.jpg",
+  },
 ];
-
-export const createAndShare = stories[0];
-export const storiesEverything = stories[1];
-export const forEveryone = stories[2];
