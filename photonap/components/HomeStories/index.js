b7b78fec1a5a28c0835d765be0634974eb63f9c5
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 15:17:04 -0400

    Fix (#41)
    * photonap project from FEM (#39)
    
    * nextjs project template setup
    
    * Styling for global header component
    
    * global footer for pages
    
    * remove not used import in header component
    
    * move assets to public folder
    
    * update name
    
    * the Hero part for features page
    
    * feature list for features page
    
    * add get invitation today for features page
    
    * adjust hero text for extreamly large screeen
    
    * adjust text size for larger screen
    
    * make the text has the same baseline for larger screen
    
    * move get invited now to footer componnent
    
    * extract FeatureHero as PageHero
    
    * hero picture for pricing page
    
    * pricing plan for pricing page
    
    * using repsonive background image for get invited section
    
    * feature pricing intro for pricing page
    
    * add title for features page
    
    * update TierCompare
    
    * extract style from component
    
    * Update index.js
    
    * Update styles.js
    
    * Update index.js
    
    * Update features.js
    
    * Update styles.js
    
    * Update pricing.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update styles.js
    
    * Update _app.js
    
    * Update features.js
    
    * Update pricing.js
    
    * the story list for stories page
    
    * fetuered story for stories page
    
    * add hover effect to story card
    
    * Update index.js
    
    * Update styles.js
    
    Co-authored-by: Kate <test@gmail.com>
    
    * extract feature list to a sinlge component for home page usage
    
    * extract style to single file
    
    * add default margin for text and arrow icon
    
    * home page
    
    * limit story list width for larger screen
    
    Co-authored-by: Kate <test@gmail.com>

---
+++ photonap/components/HomeStories/index.js
@@ -0,0 +1,53 @@
+import * as React from "react";
+import styled from "styled-components/macro";
+import * as mediaQueries from "styles/media-queries";
+import IntroText from "components/IntroText";
+import { StoryWrap, SotryImg, Wrapper } from "./styles";
+import { stories } from "./stories";
+import ButtonArrow from "components/ButtonArrow";
+
+const Story = ({ title, content, variant, name }) => {
+	const intro = { title, content, variant };
+	return (
+		<StoryWrap>
+			<SotryImg
+				css={`
+					backgrouhd-size: cover;
+					background-image: url(/home/mobile/${name});
+					${mediaQueries.medium} {
+						background-image: url(/home/tablet/${name});
+						background-position: top center;
+					}
+					${mediaQueries.large} {
+						background-image: url(/home/desktop/${name});
+					}
+				`}
+			/>
+			<IntroText {...intro} className="intro-text">
+				<ButtonArrow
+					css={`
+						margin-top: 1rem;
+						color: ${variant === "dark" ? "var(--white)" : "var(--black)"};
+						svg path {
+							stroke: ${variant === "dark"
+								? "var(--white)"
+								: "var(--black)"}!important;
+						}
+					`}
+				>
+					<span>View the story</span>
+				</ButtonArrow>
+			</IntroText>
+		</StoryWrap>
+	);
+};
+
+export default function HomeStories() {
+	return (
+		<Wrapper>
+			{stories.map((story) => (
+				<Story {...story} key={story.title} />
+			))}
+		</Wrapper>
+	);
+}
--- photonap/pages/pricing.js
+++ photonap/pages/pricing.js
@@ -7,37 +7,37 @@ import TireCompare from "components/TierCompare";
 import Head from "next/head";
 
 const intro = {
-  title: "pricing",
-  content:
-    "Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.",
+	title: "pricing",
+	content:
+		"Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.",
 };
 
 export default function PricingScreen() {
-  return (
-    <>
-      <Head>
-        <title>Photonap | Pricing</title>
-      </Head>
-      <PageHero
-        {...intro}
-        css={`
-          .hero-img {
-            background-image: url(/pricing/mobile/hero.jpg);
-          }
-          ${mediaQueries.medium} {
-            .hero-img {
-              background-image: url(/pricing/tablet/hero.jpg);
-            }
-          }
-          ${mediaQueries.large} {
-            .hero-img {
-              background-image: url(/pricing/desktop/hero.jpg);
-            }
-          }
-        `}
-      />
-      <PricingPlans />
-      <TireCompare />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Photonap | Pricing</title>
+			</Head>
+			<PageHero
+				{...intro}
+				css={`
+					.hero-img {
+						background-image: url(/pricing/mobile/hero.jpg);
+					}
+					${mediaQueries.medium} {
+						.hero-img {
+							background-image: url(/pricing/tablet/hero.jpg);
+						}
+					}
+					${mediaQueries.large} {
+						.hero-img {
+							background-image: url(/pricing/desktop/hero.jpg);
+						}
+					}
+				`}
+			/>
+			<PricingPlans />
+			<TireCompare />
+		</>
+	);
 }
