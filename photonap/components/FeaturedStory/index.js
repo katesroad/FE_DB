a4f154e6912206b4e549ceb766505b33b6bf264e
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:44:20 -0400

    Update index.js

--- photonap/components/FeaturedStory/index.js
+++ photonap/components/FeaturedStory/index.js
@@ -1,32 +1,33 @@
+
 import { story } from "./intro";
 import {
-	Wrapper,
-	HeroImg,
-	StoryIntro,
-	IntroTitle,
-	IntroContent,
-	IntroStamp,
+  Wrapper,
+  HeroImg,
+  StoryIntro,
+  IntroTitle,
+  IntroContent,
+  IntroStamp,
 } from "./styles";
 import ButtonArrow from "components/ButtonArrow";
 
 export default function FeaturedStory() {
-	return (
-		<Wrapper>
-			<HeroImg />
-			<StoryIntro>
-				<IntroTitle>
-					<h6 className="sub-title">last month's featured story</h6>
-					<h2>{story.topic}</h2>
-				</IntroTitle>
-				<IntroStamp>
-					<span className="date">{story.createdAt}</span>
-					<span className="author"> by {story.author}</span>
-				</IntroStamp>
-				<IntroContent>{story.content}</IntroContent>
-				<ButtonArrow className="btn-read">
-					<span styke={{ marginRight: "1rem" }}>read the story</span>
-				</ButtonArrow>
-			</StoryIntro>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <HeroImg />
+      <StoryIntro>
+        <IntroTitle>
+          <h6 className="sub-title">last month's featured story</h6>
+          <h2>{story.topic}</h2>
+        </IntroTitle>
+        <IntroStamp>
+          <span className="date">{story.createdAt}</span>
+          <span className="author"> by {story.author}</span>
+        </IntroStamp>
+        <IntroContent>{story.content}</IntroContent>
+        <ButtonArrow className="btn-read">
+          <span styke={{ marginRight: "1rem" }}>read the story</span>
+        </ButtonArrow>
+      </StoryIntro>
+    </Wrapper>
+  );
 }
