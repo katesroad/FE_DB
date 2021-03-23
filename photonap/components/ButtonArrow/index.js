a85b4d84a932ab68748b39e2dafa8f90e3ae9cd1
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:43:30 -0400

    Update index.js

--- photonap/components/ButtonArrow/index.js
+++ photonap/components/ButtonArrow/index.js
@@ -1,36 +1,34 @@
-import styled from "styled-components/macro";
-
 export default function ButtonArrow({ children, ...props }) {
-	return (
-		<button
-			css={`
-				display: flex;
-				align-items: center;
-				justify-content: space-between;
-				text-transform: uppercase;
-				background-color: transparent;
-				color: var(--white);
-				letter-spacing: 2px;
-			`}
-			{...props}
-		>
-			{children}
-			<span
-				css={`
-					margin-left: 1rem;
-				`}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="42"
-					height="14"
-					viewBox="0 0 42 14"
-					fill="none"
-				>
-					<path d="M0 7H41.864" stroke="white" />
-					<path d="M35.4282 1L41.4282 7L35.4282 13" stroke="white" />
-				</svg>
-			</span>
-		</button>
-	);
+  return (
+    <button
+      css={`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        text-transform: uppercase;
+        background-color: transparent;
+        color: var(--white);
+        letter-spacing: 2px;
+      `}
+      {...props}
+    >
+      {children}
+      <span
+        css={`
+          margin-left: 1rem;
+        `}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="42"
+          height="14"
+          viewBox="0 0 42 14"
+          fill="none"
+        >
+          <path d="M0 7H41.864" stroke="white" />
+          <path d="M35.4282 1L41.4282 7L35.4282 13" stroke="white" />
+        </svg>
+      </span>
+    </button>
+  );
 }
