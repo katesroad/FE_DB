c4c5ddda3f404440dbb6bb5e42d176d391cec645
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:47:40 -0400

    Update index.js

--- photonap/components/PageHero/index.js
+++ photonap/components/PageHero/index.js
@@ -3,11 +3,11 @@ import { Wrapper, HeroImg } from "./styles";
 import IntroText from "components/IntroText";
 
 export default function PageHero({ title, content, ...props }) {
-	const intro = { title, content };
-	return (
-		<Wrapper {...props}>
-			<HeroImg />
-			<IntroText variant="dark" {...intro} className="intro-text" />
-		</Wrapper>
-	);
+  const intro = { title, content };
+  return (
+    <Wrapper {...props}>
+      <HeroImg />
+      <IntroText variant="dark" {...intro} className="intro-text" />
+    </Wrapper>
+  );
 }
