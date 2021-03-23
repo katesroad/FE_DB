8d2830876b7203362d559532523d122bfaa3b184
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:47:19 -0400

    Update index.js

--- photonap/components/IntroText/index.js
+++ photonap/components/IntroText/index.js
@@ -3,32 +3,32 @@ import * as React from "react";
 import { Wrapper, Content, ColorfulLine } from "./styles";
 
 export default function IntroText({
-	title,
-	content,
-	variant,
-	children,
-	...props
+  title,
+  content,
+  variant,
+  children,
+  ...props
 }) {
-	return (
-		<Wrapper
-			css={`
-				background: ${variant === "dark" ? "var(--black)" : "var(--white)"};
-			`}
-			{...props}
-		>
-			{variant === "dark" ? <ColorfulLine /> : null}
-			<Content
-				css={`
-					color: ${variant === "dark" ? "var(--white)" : "var(--black)"};
-					p {
-						opacity: 0.6;
-					}
-				`}
-			>
-				<h2>{title}</h2>
-				<p>{content}</p>
-				{children}
-			</Content>
-		</Wrapper>
-	);
+  return (
+    <Wrapper
+      css={`
+        background: ${variant === "dark" ? "var(--black)" : "var(--white)"};
+      `}
+      {...props}
+    >
+      {variant === "dark" ? <ColorfulLine /> : null}
+      <Content
+        css={`
+          color: ${variant === "dark" ? "var(--white)" : "var(--black)"};
+          p {
+            opacity: 0.6;
+          }
+        `}
+      >
+        <h2>{title}</h2>
+        <p>{content}</p>
+        {children}
+      </Content>
+    </Wrapper>
+  );
 }
