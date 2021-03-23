347ba79e5f98a3123abf6e9003134416658cb2fa
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 16:45:40 -0400

    Update index.js

--- photonap/components/Header/index.js
+++ photonap/components/Header/index.js
@@ -4,25 +4,25 @@ import Link from "next/link";
 import { Wrapper, Nav, NavList, NavButton } from "./styles";
 
 export default function Header() {
-	const [menuIsOpen, setMenuIsOpen] = React.useState(false);
-	const handleClick = (e) => setMenuIsOpen(!menuIsOpen);
-	return (
-		<Wrapper as="header">
-			<Link href="/">
-				<img
-					src="https://fem-photosnap-app-g8glvjkbv-asaulters.vercel.app/assets/shared/desktop/logo-black.svg"
-					alt=""
-				/>
-			</Link>
-			<Nav>
-				<NavButton onClick={handleClick}>{menuIsOpen ? "x" : "="}</NavButton>
-				<NavList className={menuIsOpen ? "open" : "close"}>
-					<li className="gray-line"></li>
-					<li>
-						<Button>GET AN INVITATION</Button>
-					</li>
-				</NavList>
-			</Nav>
-		</Wrapper>
-	);
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+  const handleClick = (e) => setMenuIsOpen(!menuIsOpen);
+  return (
+    <Wrapper as="header">
+      <Link href="/">
+        <img
+          src="https://fem-photosnap-app-g8glvjkbv-asaulters.vercel.app/assets/shared/desktop/logo-black.svg"
+          alt=""
+        />
+      </Link>
+      <Nav>
+        <NavButton onClick={handleClick}>{menuIsOpen ? "x" : "="}</NavButton>
+        <NavList className={menuIsOpen ? "open" : "close"}>
+          <li className="gray-line"></li>
+          <li>
+            <Button>GET AN INVITATION</Button>
+          </li>
+        </NavList>
+      </Nav>
+    </Wrapper>
+  );
 }
