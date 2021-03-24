4a7684b0d5d7cd6f32ff51a3eeb96ddb3aefb3a0
Kate <3837437+katesroad@users.noreply.github.com>
2021-03-23 22:44:46 -0400

    Update features.js

--- photonap/pages/features.js
+++ photonap/pages/features.js
@@ -8,38 +8,38 @@ import Head from "next/head";
 import FeatureList from "components/FeatureList";
 
 const intro = {
-	title: "features",
-	content: `We make sure all of our features are designed to be loved by every
+  title: "features",
+  content: `We make sure all of our features are designed to be loved by every
 					aspiring and even professional photograpers who wanted to share their
 					stories.`,
 };
 export default function FeaturesScreen() {
-	return (
-		<>
-			<Head>
-				<title>Photonap | Features</title>
-			</Head>
-			<PageHero
-				{...intro}
-				css={`
-					.hero-img {
-						background-image: url(/features/mobile/hero.jpg);
-					}
-					${mediaQueries.medium} {
-						.hero-img {
-							background-image: url(/features/tablet/hero.jpg);
-						}
-					}
-					${mediaQueries.large} {
-						.hero-img {
-							background-image: url(/features/desktop/hero.jpg);
-						}
-					}
-				`}
-			/>
-			<Content>
-				<FeatureList features={features} />
-			</Content>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Photonap | Features</title>
+      </Head>
+      <PageHero
+        {...intro}
+        css={`
+          .hero-img {
+            background-image: url(/features/mobile/hero.jpg);
+          }
+          ${mediaQueries.medium} {
+            .hero-img {
+              background-image: url(/features/tablet/hero.jpg);
+            }
+          }
+          ${mediaQueries.large} {
+            .hero-img {
+              background-image: url(/features/desktop/hero.jpg);
+            }
+          }
+        `}
+      />
+      <Content>
+        <FeatureList features={features} />
+      </Content>
+    </>
+  );
 }
