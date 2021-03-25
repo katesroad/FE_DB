d56735c3312f6e0a0a85bd527159ca9ee8b8a5a6
Kate <test@gmail.com>
2021-03-25 01:00:39 -0400

    site footer

--- pay/components/Footer/index.js
+++ pay/components/Footer/index.js
@@ -1,5 +1,26 @@
+import { Facebook, Logo, Twitter, Linkedin } from 'components/Icons';
+import { NavItem } from 'components/lib';
 import * as React from 'react';
+import { Footer, SocialIcons, NavItems, FooterContent, Column } from './styles';
 
-export default function Footer() {
-  return null;
+export default function PageFooter() {
+  return (
+    <Footer>
+      <FooterContent>
+        <Column>
+          <Logo />
+          <NavItems>
+            <NavItem href="/pricing">Pricing</NavItem>
+            <NavItem href="/about">about</NavItem>
+            <NavItem href="/contact">Contact</NavItem>
+          </NavItems>
+        </Column>
+        <SocialIcons>
+          <Facebook />
+          <Twitter />
+          <Linkedin />
+        </SocialIcons>
+      </FooterContent>
+    </Footer>
+  );
 }
--- pay/components/Footer/styles.js
+++ pay/components/Footer/styles.js
@@ -2,42 +2,82 @@ import { Content } from 'components/lib';
 import styled from 'styled-components';
 import * as mediaQueries from 'styles/media-queries';
 
-export const Wrapper = styled.div`
-  padding-bottom: 6.25rem;
-  padding-top: 6.25rem;
-  background-color: var(--white);
+export const Footer = styled.footer`
+  padding-top: 2.56rem;
+  padding-bottom: 3.4375rem;
+  background-color: var(--c11);
+  background-image: url(/shared/desktop/bg-pattern-circle.svg);
+  background-position: center -39%;
+  background-repeat: no-repeat;
   ${mediaQueries.medium} {
-    padding-top: 4.375rem;
-  }
-  ${mediaQueries.large} {
-    position: relative;
-    padding-top: 7.5rem
-    padding-bottom: 7.1875rem;
-    background-image: url(assets/desktop/bg-pattern-wave-red.svg);
-    background-position: -95px center;
-    background-repeat: no-repeat;
+    padding-top: 1.875rem;
+    padding-bottom: 1.875rem;
+    background-position: center right;
+    background-size: 56%;
+    background-position: 110% -40%;
   }
 `;
 
 export const FooterContent = styled(Content)`
   display: flex;
   flex-direction: column;
-  h4 {
-    font-size: 2rem;
-    line-height: 1.25;
-  }
-  .btn--red {
-    height: 4rem;
-    width: 12.5rem;
-    padding: 1.5rem 0;
-    margin-top: 3rem;
-  }
+  align-items: center;
   ${mediaQueries.medium} {
     flex-direction: row;
-    align-items: center;
     justify-content: space-between;
-    .btn--red {
-      margin-top: 0;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${mediaQueries.medium} {
+    flex-direction: row;
+  }
+`;
+
+export const NavItems = styled.p.attrs(() => ({ className: 'nav-items' }))`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 2.5rem;
+  padding-bottom: 2.5rem;
+  a {
+    font-size: 0.9375rem;
+    text-transform: capitalize;
+    color: #fbfcfe;
+    opacity: 0.7;
+    font-weight: var(--font-bold);
+    &:hover {
+      opacity: 1;
+    }
+    &:nth-child(2n) {
+      margin-bottom: 1.875rem;
+      margin-top: 1.875rem;
+    }
+  }
+  ${mediaQueries.medium} {
+    padding-top: 0;
+    padding-bottom: 0;
+    margin-left: 4rem;
+    flex-direction: row;
+    a:nth-child(2n) {
+      margin: 0 2.5rem;
+    }
+  }
+`;
+
+export const SocialIcons = styled.p.attrs(() => ({ className: 'social-icons' }))`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  svg {
+    cursor: pointer;
+    &:nth-child(2n) {
+      margin-left: 1.5rem;
+      margin-right: 1.5rem;
     }
   }
 `;
