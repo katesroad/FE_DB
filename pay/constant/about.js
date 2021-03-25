90fb186d9356d26953d3a255538ec459a73f75f6
Kate <test@gmail.com>
2021-03-25 01:22:04 -0400

    text content for about page

---
+++ pay/constant/about.js
@@ -0,0 +1,44 @@
+export const thePeople = {
+  title: 'the people',
+  content: ` We're all passionate about building a more efficient and inclusive financial infrastructure 
+    together. At PayAPI, we have diverse backgrounds and skills.`
+};
+
+export const theCultuer = {
+  title: 'the culture',
+  content: `We strongly believe there's always an opportunity to learn from each other outside of 
+    day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. 
+    We always value cross-team collaboration and diversity of thought, no matter the job title.`
+};
+
+export const ourBusiness = {
+  title: 'our business',
+  content: ` At the core of our platform is the technical infrastructure APIs that connect consumers. 
+    Our innovative product provides key insights for businesses and individuals, as well as 
+    robust reporting for traditional financial institutions and developers.`
+};
+
+export const ourVision = {
+  title: 'our vision',
+  content: ` Our main goal is to build beautiful consumer experiences along with developer-friendly 
+    infrastructure. The result is an intelligent tool that gives everyone the ability to create 
+    amazing products that solve big problems. We are deeply focused on democratizing financial 
+    services through technology.`
+};
+
+export const slogan = ` We empower innovators by delivering access to the financial system`;
+
+export const ourStats = [
+  {
+    name: 'team members',
+    data: '300+'
+  },
+  {
+    name: 'offices in the US',
+    data: '3'
+  },
+  {
+    name: 'Transations analyzed',
+    data: '10M+'
+  }
+];
