fa95e8538ebb667e75c6c424a592162b353ac9ae
Kate <test@gmail.com>
2021-03-26 23:06:35 -0400

    client list at home page

--- pay/pages/index.js
+++ pay/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import UseCaseList from 'components/UseCases';
 import Head from 'next/head';
+import OurClients from 'components/OurClients';
 
 export default function IndexScreen() {
   return (
@@ -8,6 +9,7 @@ export default function IndexScreen() {
       <Head>
         <title>Pay API</title>
       </Head>
+      <OurClients />
       <UseCaseList />
     </>
   );
