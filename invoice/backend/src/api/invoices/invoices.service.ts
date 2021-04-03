70806825dd1a8323fd6c5fc3082e0ef905917acd
Kate <test@gmail.com>
2021-04-02 23:28:05 -0400

    add authentication for invoices endpoint

--- invoice/backend/src/api/invoices/invoices.service.ts
+++ invoice/backend/src/api/invoices/invoices.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Invoice, InvoiceDoc } from 'mongo';
-import { Model } from 'mongoose';
+import { Model, Mongoose } from 'mongoose';
 import { HelperService } from './helper.service';
 
 @Injectable()
@@ -12,10 +12,10 @@ export class InvoicesService {
     private readonly helperService: HelperService,
   ) {}
 
-  getInvoices(filterStatus?: string) {
-    let filter: any = null;
+  getInvoices(userId: string, filterStatus?: string) {
+    let filter: any = { author: userId };
     if (filterStatus) {
-      filter = { status: { $in: filterStatus.split(',') } };
+      filter.status = { $in: filterStatus.split(',') };
     }
     return this.invoiceModel
       .find(filter, {
@@ -24,43 +24,51 @@ export class InvoicesService {
         clientName: 1,
         tag: 1,
         total: 1,
+        author: 1,
       })
       .then((docs) => docs.map((doc) => this.cleanDoc(doc)));
   }
 
-  getInvoice(id: string) {
-    return this.invoiceModel.findOne({ _id: id }).then((doc) => {
-      if (!doc)
-        throw new BadRequestException(`Can't find invoice with id#${id}`);
-      return this.cleanDoc(doc);
-    });
+  getInvoice(userId: string, id: string) {
+    return this.invoiceModel
+      .findOne({ _id: id, author: userId })
+      .then((doc) => {
+        if (!doc)
+          throw new BadRequestException(`Can't find invoice with id#${id}`);
+        return this.cleanDoc(doc);
+      });
   }
 
-  createInvoice(createInvoiceDto: any) {
+  createInvoice(userId: string, createInvoiceDto: any) {
     const items = createInvoiceDto.items;
     const tag = this.helperService.makeTag();
-    const data = { ...createInvoiceDto, tag, ...this.calcTotal(items) };
+    const data = {
+      ...createInvoiceDto,
+      tag,
+      ...this.calcTotal(items),
+      author: userId,
+    };
     return this.invoiceModel
       .create(data)
       .then((doc) => this.cleanDoc(doc))
       .catch((e) => e);
   }
 
-  updateInvoice(id: string, updateInvoiceDto: any) {
+  updateInvoice(userId: string, id: string, updateInvoiceDto: any) {
     let { items, ...update } = updateInvoiceDto;
     if (items) {
       update = { ...update, ...this.calcTotal(items) };
     }
     return this.invoiceModel
-      .updateOne({ _id: id }, update)
-      .then(() => this.invoiceModel.findOne({ _id: id }))
+      .findOneAndUpdate({ _id: id, author: userId }, update)
       .then((doc) => {
         if (doc) return this.cleanDoc(doc);
-        else throw new BadRequestException(`Can't find invoice#${id}`);
+        else
+          throw new BadRequestException(`Can't find invoice#${id} for user.`);
       });
   }
 
-  deleteInvoice(id: string) {
+  deleteInvoice(userId: string, id: string) {
     return this.invoiceModel.findOneAndDelete({ _id: id });
   }
 
