cdd500ca994733ae7b843368cc588428461635bf
Kate <test@gmail.com>
2021-04-05 10:02:23 -0400

    remove duplicated props

--- invoice/frontend/src/components/Field/index.js
+++ invoice/frontend/src/components/Field/index.js
@@ -4,37 +4,32 @@ import { useField, ErrorMessage } from "formik";
 import { Label, FormControl, FieldError, Input } from "components/lib/form";
 
 const TextField = React.memo(({ label, name, children, ...props }) => {
-  const [field, meta] = useField({ name, ...props });
-  const error = meta.touched && meta.error;
-  return (
-    <FormControl>
-      <Label htmlFor={name}>{label}</Label>
-      <Input
-        {...field}
-        {...props}
-        id={name}
-        name={name}
-        className={error ? "error" : ""}
-      />
-      <FieldError>
-        <ErrorMessage name={name} />
-      </FieldError>
-      {children}
-    </FormControl>
-  );
+	const [field, meta] = useField({ name, ...props });
+	const error = meta.touched && meta.error;
+	return (
+		<FormControl>
+			<Label htmlFor={name}>{label}</Label>
+			<Input {...props} {...field} id={name} className={error ? "error" : ""} />
+			<FieldError>
+				<ErrorMessage name={name} />
+			</FieldError>
+			{children}
+		</FormControl>
+	);
 });
 TextField.defaultPropTypes = {
-  type: "text",
-  value: "",
+	type: "text",
+	value: "",
 };
 TextField.propTypes = {
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node),
-  ]),
+	label: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	type: PropTypes.oneOf(["password", "text"]),
+	children: PropTypes.oneOfType([
+		PropTypes.node,
+		PropTypes.arrayOf(PropTypes.node),
+	]),
 };
 
 export default TextField;
