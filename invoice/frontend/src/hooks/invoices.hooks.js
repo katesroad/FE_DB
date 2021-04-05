b2f3f400e550dedd5d98b421a2714bbbf0ce1a99
Kate <test@gmail.com>
2021-04-05 13:04:50 -0400

    for fetching invoices with the status filter, set staleTime as zero

--- invoice/frontend/src/hooks/invoices.hooks.js
+++ invoice/frontend/src/hooks/invoices.hooks.js
@@ -6,12 +6,13 @@ import axios from "utils/axios";
  * @param{string} - status invoice's status(pending, paid, draft)
  */
 export function useInvoices(status = "") {
-  return useQuery(
-    ["invoices", status],
-    ({ queryKey: [, status] }) => {
-      const params = status === "" ? {} : { status };
-      return axios.get("invoices", { params });
-    },
-    { staleTime: 30 * 60 * 1000 }
-  );
+	const staleTime = status ? 0 : 30 * 60 * 1000;
+	return useQuery(
+		["invoices", status],
+		({ queryKey: [, status] }) => {
+			const params = status === "" ? {} : { status };
+			return axios.get("invoices", { params });
+		},
+		{ staleTime }
+	);
 }
--- invoice/frontend/src/screens/invoice/components/Operations/index.js
+++ invoice/frontend/src/screens/invoice/components/Operations/index.js
@@ -8,21 +8,21 @@ const EditInvoice = React.lazy(() => import("../EditInvoice"));
 const DeleteInvoice = React.lazy(() => import("../DeleteInvoice"));
 
 function MarkInvoiceBtn({ id, tag, toStatus, ...props }) {
-  const { status, mutate } = useUpdateInvoice({ id, tag, toStatus });
-  return (
-    <Button
-      onClick={() => mutate({ id, status: toStatus })}
-      variant="primary"
-      {...props}
-    >
-      {status === "pending" ? "marking" : `mark as ${toStatus}`}
-    </Button>
-  );
+	const { status, mutate } = useUpdateInvoice({ id, tag });
+	return (
+		<Button
+			onClick={() => mutate({ id, status: toStatus })}
+			variant="primary"
+			{...props}
+		>
+			{status === "pending" ? "marking" : `mark as ${toStatus}`}
+		</Button>
+	);
 }
 MarkInvoiceBtn.propTypes = {
-  toStatus: PropTypes.oneOf(["pending", "paid"]),
-  id: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
+	toStatus: PropTypes.oneOf(["pending", "paid"]),
+	id: PropTypes.string.isRequired,
+	tag: PropTypes.string.isRequired,
 };
 
 /**
@@ -32,26 +32,26 @@ MarkInvoiceBtn.propTypes = {
  * An invoice can be deleteable when it has either draft or pending status
  */
 function Operations({ invoice }) {
-  if (invoice.status === "paid")
-    return (
-      <SuspenseBoundary FallbackComponent={ErrorFallback}>
-        <DeleteInvoice {...invoice} />
-      </SuspenseBoundary>
-    );
-  if (invoice.status === "draft")
-    return (
-      <>
-        <SuspenseBoundary>
-          {invoice ? <EditInvoice invoice={invoice} /> : null}
-        </SuspenseBoundary>
-        <MarkInvoiceBtn {...invoice} toStatus="pending" />
-        <MarkInvoiceBtn {...invoice} toStatus="paid" />
-      </>
-    );
-  if (invoice.status === "pending") {
-    return <MarkInvoiceBtn {...invoice} toStatus="paid" />;
-  }
-  return null;
+	if (invoice.status === "paid")
+		return (
+			<SuspenseBoundary FallbackComponent={ErrorFallback}>
+				<DeleteInvoice {...invoice} />
+			</SuspenseBoundary>
+		);
+	if (invoice.status === "draft")
+		return (
+			<>
+				<SuspenseBoundary>
+					{invoice ? <EditInvoice invoice={invoice} /> : null}
+				</SuspenseBoundary>
+				<MarkInvoiceBtn {...invoice} toStatus="pending" />
+				<MarkInvoiceBtn {...invoice} toStatus="paid" />
+			</>
+		);
+	if (invoice.status === "pending") {
+		return <MarkInvoiceBtn {...invoice} toStatus="paid" />;
+	}
+	return null;
 }
 
 export default Operations;
