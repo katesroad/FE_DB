6a7cf446ab3d00e344928d186cb928bb404e963f
Kate <3837437+katesroad@users.noreply.github.com>
2021-04-05 12:51:28 -0400

    Update login.js

--- invoice/frontend/src/screens/login.js
+++ invoice/frontend/src/screens/login.js
@@ -3,10 +3,10 @@ import AuthForm from "components/AuthForm";
 import { useLogin } from "hooks/auth.hooks";
 
 export default function LoginScreen() {
-	React.useEffect(() => {
-		document.title = "Login";
-		return () => (document.title = "FEM Invoice");
-	}, []);
-	const mutation = useLogin();
-	return <AuthForm type="login" mutation={mutation} />;
+  React.useEffect(() => {
+    document.title = "Login";
+    return () => (document.title = "FEM Invoice");
+  }, []);
+  const mutation = useLogin();
+  return <AuthForm type="login" mutation={mutation} />;
 }
