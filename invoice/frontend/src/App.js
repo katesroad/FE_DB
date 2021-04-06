dccb009c88fb7825bc483aa8162fd4fe2587c72b
Kate <3837437+katesroad@users.noreply.github.com>
2021-04-06 15:38:32 -0400

    Update App.js

--- invoice/frontend/src/App.js
+++ invoice/frontend/src/App.js
@@ -4,9 +4,9 @@ import AuthedApp from "app.authed";
 import UnAuthedApp from "app.unAuthed";
 
 function App() {
-	const { user } = useAuth();
-	const [redirectTo] = React.useState(() => window.location.pathname);
-	return user ? <AuthedApp redirectTo={redirectTo} /> : <UnAuthedApp />;
+  const { user } = useAuth();
+  const [redirectTo] = React.useState(() => window.location.pathname);
+  return user ? <AuthedApp redirectTo={redirectTo} /> : <UnAuthedApp />;
 }
 
 export default App;
